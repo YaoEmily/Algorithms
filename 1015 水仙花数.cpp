2c79f69d0f42569764d1019563fd53c825d12408
xuheyao <xuheyao_emily@163.com>
2017-10-20 22:38:56 +0800

    BUG修正

--- 1015 水仙花数.cpp
+++ 1015 水仙花数.cpp
@@ -9,15 +9,21 @@ int main(void)
     while(cin >> N)
     {
         int n;
-        int tmp, a, sum;
+        int tmp, a, sum, cycle;
         for(n = N; ; n++)
         {
             a = n;
             sum = 0;
+            cycle = 0;
+            while(a){
+                cycle++;
+                a /= 10;
+            }
+            a = n;
             while(a)
             {
                 tmp = a % 10;
-                sum += tmp * tmp * tmp;
+                sum += pow(tmp, cycle);
                 a /= 10;
             }
             if(sum == n){
