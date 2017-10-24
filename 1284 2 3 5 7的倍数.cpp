05c968202c3ac7017d4509812ff927ac2bf49800
xuheyao <xuheyao_emily@163.com>
2017-10-24 09:22:18 +0800

    修改BUG

--- 1284 2 3 5 7的倍数.cpp
+++ 1284 2 3 5 7的倍数.cpp
@@ -4,7 +4,7 @@ using namespace std;
 long long N;
 
 int main(void){
-    int a;
+    long long a;
     while(cin >> N){
         /*»›≥‚‘≠¿Ì*/
         a = N - (N/2 + N/3 + N/5 + N/7) + (N/6 + N/10 + N/14 + N/15 + N/21 + N/35) - (N/30 + N/70 + N/42 + N/105) + N/210;
