5677f43cd9b1bb9f7e767e29e1085f2cf1bb5422
xuheyao <xuheyao_emily@163.com>
2017-10-29 22:39:35 +0800

    数位DP计算完毕

--- 1009 数字1的数量.cpp
+++ 1009 数字1的数量.cpp
@@ -4,14 +4,39 @@
 using namespace std;
 
 int N;
-int dp[11][11];
+int arr[10][10];
 
 int main(void){
     while(cin >> N){
-        arr[1] = 1;
-        for(int i = 2; i < 10; i++){
-            arr[i] = pow(10, (i-1)) + arr[i-1] * 8;
-            cout<<arr[i]<<endl;
+        int sum = 0;
+        for(int i = 1; i < 10; i++){
+            for(int j = 0; j < 10; j++){
+                if(i == 1){
+                    arr[i][j] = 0;
+                    if(j == 1){
+                        arr[i][j] = 1;
+                    }
+                }
+                if(j == 0){
+                    arr[i][j] = sum;
+                    sum = 0;
+                }else if(j == 1){
+                    int tmp = 1;
+                    for(int k = 0; k < i-1; k++){
+                        tmp *= 10;
+                    }
+                    arr[i][j] = tmp + arr[i][0];
+                }else {
+                    arr[i][j] = arr[i][0];
+                }
+                sum += arr[i][j];
+            }
+        }
+        for(int i = 1; i < 10; i++){
+            for(int j = 0; j < 10; j++){
+                cout<<arr[i][j]<<" ";
+            }
+            cout<<endl;
         }
     }
 }
