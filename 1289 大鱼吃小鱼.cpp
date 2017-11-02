eee32b626a6e5aff06e84a011c6babc4a6e8af12
xuheyao <xuheyao_emily@163.com>
2017-11-02 20:42:36 +0800

    还没A 大致思路应该是对的

--- 1289 大鱼吃小鱼.cpp
+++ 1289 大鱼吃小鱼.cpp
@@ -6,22 +6,34 @@ int arr[100001][2];
 
 int main(void){
     while(cin >> N){
-        for(int i = 0; i < N; i++){
-            cin >> arr[i][0] >> arr[i][1];
-        }
-        int left, right;
-        for(int i = 0; i < N - 1; ){
-            left = i;
-            right = i + 1;
-            if(arr[left][1] == 1 && arr[right][1] == 0){
-                if(arr[left][0] > arr[right][0]){
-                    arr[right][0] = 0;
-                    right++;
+        int a, b;
+        int sum = 0;
+        while(N--){
+            cin >> arr[sum][0] >> arr[sum][1];
+            if(sum == 0 || arr[sum-1][1] == 0 ||(arr[sum-1][1] == 1 && arr[sum][1] == 1)){
+                /*第一个或者全都向左游 将该组数据写入 sum加1*/
+                sum++;
+                //cout << "first" <<sum<< endl;
+            }else if(arr[sum-1][1] == 1 && arr[sum][1] == 0){
+                /*上一组向右 该组向左 吃小鱼处理*/
+                if(arr[sum-1][0] > arr[sum][0]){
+                    /*左鱼大于右鱼 吃掉右鱼 sum不做处理*/
+                    //cout << "second" <<sum<< endl;
                 }else{
-                    arr[left][0] = 0;
-                    left--;
+                    /*左鱼小于右鱼 吃掉左鱼 并迭代*/
+                    if(arr[sum-1][0] < arr[sum][0]){
+                        arr[sum-1][0] = arr[sum][0];
+                        arr[sum-1][1] = arr[sum][1];
+                    }
+                    while(sum >= 2 && arr[sum-2][1] == 1 && (arr[sum-2][0] < arr[sum-1][0])){
+                        arr[sum-2][0] = arr[sum-1][0];
+                        arr[sum-2][1] = arr[sum-1][1];
+                        sum--;
+                        //cout << "third" <<sum<< endl;
+                    }
                 }
             }
         }
+        cout << sum <<endl;
     }
 }
