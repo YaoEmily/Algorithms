8fb788a2b4d29c11627bb2440d2c0f73ec65b232
xuheyao <xuheyao_emily@163.com>
2017-11-08 09:01:37 +0800

    1181 质数中的质数（质数筛法）
    注意相等的情况

--- 1181 质数中的质数（质数筛法）.cpp
+++ 1181 质数中的质数（质数筛法）.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 #include <string.h>
+#define MAXSIZE 1001000
 using namespace std;
 
 int N;
-int prime[1000001];
-int mark[1000001];
-int prime2[1000001];
+int prime[MAXSIZE];
+int mark[MAXSIZE];
+int prime2[MAXSIZE];
 
 int main(void){
     while(cin >> N){
         memset(mark, 0, sizeof(mark[0]));
         int index = 0;
-        for(int i = 2; i < 1000001; i++){
+        for(int i = 2; i < MAXSIZE; i++){
             if(mark[i] == 0){
                 prime[index++] = i;
             }
-            for(int j = 0; j < index && prime[j] * i < 1000001; j++){
+            for(int j = 0; j < index && prime[j] * i < MAXSIZE; j++){
                 mark[i*prime[j]] = 1;
                 if(i % prime[j] == 0){
                     break;
@@ -25,7 +26,7 @@ int main(void){
         int index2 = 0;
         for(int i = 0; i < index; i++){
             prime2[index2] = prime[prime[i]-1];
-            if(prime2[index2] > N){
+            if(prime2[index2] >= N){
                 cout<<prime2[index2]<<endl;
                 break;
             }
